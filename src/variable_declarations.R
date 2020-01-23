6865d9f0805eb4996f9e75a1ac0357f7a1f93631
HypoChloremic <26204559+HypoChloremic@users.noreply.github.com>
2020-01-23 17:16:32 +0100

    src folder and variable_declarations

---
+++ src/variable_declarations.R
@@ -0,0 +1,15 @@
+# There seemingly are several different operators that can 
+# be used for declaring variables.
+# there is the "<-", and "=", "->"
+# ‘-> ->>’           rightwards assignment
+# ‘<- <<-’           assignment (right to left)
+# ‘=’                assignment (right to left)
+#
+# but technically, the <- has the same function as the = operator
+# So I will use the =. 
+
+# foo set to equal 2
+foo = 2
+
+# bar set to equal 2
+bar <- 2
